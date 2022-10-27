af9071564572ae378713296b196d3dc211a3cc74
Rodrigo Nicolas <rodrinb@hotmail.com>
2022-10-27 12:05:04 -0300

    formulario con javascript

---
+++ index.js
@@ -0,0 +1,13 @@
+const inputs = document.getElementsByClassName('formulario-input');
+for (let i = 0; i < inputs.length; i++){
+    inputs[i].addEventListener('keyup', function() {
+        if(this.value.length >=1 ){
+            this.nextElementSibling.classList.add('fijar');
+
+        } else {
+            this.nextElementSibling.classList.remove('fijar');
+
+        }
+
+    });
+}
